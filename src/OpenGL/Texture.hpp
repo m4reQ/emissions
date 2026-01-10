8085c08f5312a8ce211f326494ff65b44c845f8b
m4reQ <m.mizerski2004@gmail.com>
2026-01-10 13:00:34 +0100

    Add texture constructor that takes in texture size as glm vector

--- src/OpenGL/Texture.hpp
+++ src/OpenGL/Texture.hpp
@@ -1,12 +1,15 @@
 #pragma once
 #include <utility>
 #include <glad/gl.h>
+#include <glm/vec2.hpp>
 
 class Texture2D
 {
 public:
     Texture2D() = default;
     Texture2D(GLsizei width, GLsizei height, GLenum format, GLsizei levels = 1);
+    Texture2D(const glm::ivec2 &size, GLenum format, GLsizei levels = 1)
+        : Texture2D(size.x, size.y, format, levels) { }
     Texture2D(const Texture2D&) = delete;
     Texture2D(Texture2D&& other) noexcept;
     
